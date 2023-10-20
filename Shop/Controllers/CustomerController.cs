91663638aade6edcae7c27d1f19474e430c99300
Sumaya21 <sumayajahankanta@gmail.com>
2023-10-20 22:46:07 +0600

    Add project files.

---
+++ Shop/Controllers/CustomerController.cs
@@ -0,0 +1,124 @@
+﻿using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public List<CartDto> cart
+        {
+            get
+            {
+                if (Session["cart"] == null)
+                {
+                    Session["cart"] = new List<CartDto>();
+                    return (List<CartDto>)Session["cart"];
+                }
+                return (List<CartDto>)Session["cart"];
+            }
+            set
+            {
+                Session["cart"] = value;
+            }
+        }
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var db = new ProductSystemEntities2();
+            var data=db.Products.ToList();
+            return View(data);
+        }
+
+
+        public ActionResult AddToCart(int Id) 
+        {
+
+            var db = new ProductSystemEntities2();
+
+            var product = db.Products.Find(Id);
+            CartDto p = new CartDto()
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Price = product.Price
+            };
+
+            cart.Add(p);
+            
+            if (cart != null)
+            {
+                TempData["msg"] = "Product Added";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["msg"] = "Add Failed";
+                return RedirectToAction("Index");
+            }
+
+
+        }
+        public ActionResult Cart()
+        {
+            var data = cart.ToList();
+            double total = 0;
+            foreach (var item in cart)
+            {
+                total += Double.Parse(item.Price);
+            }
+            ViewBag.TotalPrice = total;
+
+            return View(data);
+        }
+
+        public ActionResult DeleteCart(int Id)
+        {
+            var obj = cart.FirstOrDefault(item => item.Id == Id);
+            if (obj != null)
+            {
+                cart.Remove(obj);
+                TempData["msg"] = "Item Removed";
+                return RedirectToAction("Cart");
+            }
+            else
+            {
+                TempData["msg"] = "Remove Failed";
+                return RedirectToAction("Cart");
+            }
+        }
+
+        public ActionResult Order()
+        {
+            var db = new ProductSystemEntities2();
+            var o = new Oder()
+            {
+                CustomersId = (int)Session["Id"],
+                Date = DateTime.Now
+            };
+
+            db.Oders.Add(o);
+            db.SaveChanges();
+
+            foreach (var item in cart)
+            {
+                var od = new OdersDetail()
+                {
+                    OdersId = o.Id,
+                    ProductsId = item.Id,
+                };
+
+                db.OdersDetails.Add(od);
+                db.SaveChanges();
+            }
+
+            cart.Clear();
+            TempData["msg"] = "Order Completed";
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Shop/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult Signup() 
+        {
+        
+        return View();
+        }
+
+        public ActionResult Signup(CustomersDto obj)
+        {
+            var db = new ProductSystemEntities2();
+
+            if (ModelState.IsValid)
+            {
+                Customer c = new Customer()
+                {
+                    Name = obj.Name,
+                    Email = obj.Email,
+                    Password = obj.Password
+                };
+
+
+
+                db.Customers.Add(c);
+                db.SaveChanges();
+                TempData["msg"] = "User Added!";
+                return RedirectToAction("Login", "Home");
+            }
+
+            else
+            {
+                TempData["msg"] = "Signup failed";
+                return View();
+            }
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginDto obj)
+        {
+            var db = new ProductSystemEntities2();
+
+            var customers = (from c in db.Customers
+                            where c.Email.Equals(obj.Email) &&
+                            c.Password.Equals(obj.Password)
+                            select c).SingleOrDefault();
+
+            if (customers != null)
+            {
+                Session["Id"] = customers.Id;
+                Session["Name"] = customers.Name;
+                Session["Email"] = customers.Email;
+
+                return RedirectToAction("Index", "Customer");
+            }
+
+            else
+            {
+                TempData["msg"] = "Login Failed!";
+                return View(obj);
+            }
+
+
+
+        }
+    }
+
+    
+}
